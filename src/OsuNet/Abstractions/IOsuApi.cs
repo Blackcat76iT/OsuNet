23df87a4415c1e8515e40f9573bf35fd94e64a99
Blackcat76iT <Buzakov.Kiril1@yandex.ru>
2023-03-23 16:52:16 +0300

    feat: добавил асинхронные методы в IOsuApi

--- src/OsuNet/Abstractions/IOsuApi.cs
+++ src/OsuNet/Abstractions/IOsuApi.cs
@@ -6,11 +6,18 @@ namespace OsuNet.Abstractions {
         string AccessToken { get; set; }
 
         Beatmap[] GetBeatmap(GetBeatmapOptions options);
+        Task<Beatmap[]> GetBeatmapAsync(GetBeatmapOptions options);
         User[] GetUser(GetUserOptions options);
+        Task<User[]> GetUserAsync(GetUserOptions options);
         Scores[] GetScore(GetScoresOptions options);
+        Task<Scores[]> GetScoreAsync(GetScoresOptions options);
         UserBest[] GetUserBest(GetUserBestOptions options);
+        Task<UserBest[]> GetUserBestAsync(GetUserBestOptions options);
         UserRecent[] GetUserRecent(GetUserRecentOptions options);
+        Task<UserRecent[]> GetUserRecentAsync(GetUserRecentOptions options);
         Multiplayer GetMultiplayer(GetMultiplayerOptions options);
+        Task<Multiplayer> GetMultiplayerAsync(GetMultiplayerOptions options);
         Replay GetReplay(GetReplayOptions options);
+        Task<Replay> GetReplayAsync(GetReplayOptions options);
     }
 }
