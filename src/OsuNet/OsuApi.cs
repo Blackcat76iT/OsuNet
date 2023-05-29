2377778ce428461c04824590eef0d0d10b0729ed
Blackcat76iT <Buzakov.Kiril1@yandex.ru>
2023-05-29 07:03:28 +0300

    refactior!: separating dictionaries from methods
    BREAKING CHANGE: fixed a typo in the name of the GetScore/GetScoresAsync method, now it's GetScores/GetScoresAsync

--- src/OsuNet/OsuApi.cs
+++ src/OsuNet/OsuApi.cs
@@ -54,6 +54,83 @@ namespace OsuNet {
 			}
 		}
 
+		private Dictionary<string, string> BeatmapQuery(GetBeatmapOptions options) {
+			return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "since", options.Since?.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss") },
+                { "s", options.BeatmapSetId?.ToString() },
+                { "b", options.BeatmapId?.ToString() },
+                { "u", options.UserId?.ToString() },
+                { "type", options.Type },
+                { "m", ((int?)options.Mode)?.ToString() },
+                { "a", options.ConvertedBeatmaps == true ? "1" : "0" },
+                { "h", options.Hash },
+                { "limit", options.Limit?.ToString() },
+                { "mods", options.Mods?.ToString() }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+		private Dictionary<string, string> UserQuery(GetUserOptions options) {
+            return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "u", options.User.ToString() },
+                { "m", ((int)options.Mode).ToString() },
+                { "type", options.Type },
+                { "event_days", options.EventDays?.ToString() }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+		private Dictionary<string, string> UserBestQuery(GetUserBestOptions options) {
+			return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "u", options.User },
+                { "m", ((int?)options.Mode)?.ToString() },
+                { "limit", options.Limit?.ToString() },
+                { "type", options.Type }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+		private Dictionary<string, string> UserRecentQuery(GetUserRecentOptions options) {
+            return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "u", options.User },
+                { "m", ((int?)options.Mode)?.ToString() },
+                { "limit", options.Limit?.ToString() },
+                { "type", options.Type}
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        private Dictionary<string, string> ScoresQuery(GetScoresOptions options) {
+            return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "b", options.BeatmapId?.ToString() },
+                { "u", options.User },
+                { "m", ((int)options.Mode).ToString() },
+                { "mods", options.Mods?.ToString() },
+                { "type", options.Type},
+                { "limit", options.Limit?.ToString() }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        private Dictionary<string, string> MultiplayerQuery(GetMultiplayerOptions options) {
+            return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "mp", options.MatchId.ToString() }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        private Dictionary<string, string> ReplayQuery(GetReplayOptions options) {
+            return new Dictionary<string, string>() {
+                { "k", AccessToken },
+                { "b", options.BeatmapId.ToString() },
+                { "u", options.User },
+                { "m", ((int?)options.Mode)?.ToString() },
+                { "s", options.ScoreId },
+                { "type", options.Type },
+                { "mods", ((int?)options.Mods)?.ToString() }
+            }.Where(kv => kv.Value != null).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
 		/// <summary>
 		/// Retrieve general beatmap information.
 		/// </summary>
@@ -62,20 +139,7 @@ namespace OsuNet {
 		[Obsolete("Use GetBeatmapAsync instead")]
 		public Beatmap[] GetBeatmap(GetBeatmapOptions options) {
 			string url = "https://osu.ppy.sh/api/get_beatmaps";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "since", options.Since?.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss") },
-				{ "s", options.BeatmapSetId?.ToString() },
-				{ "b", options.BeatmapId?.ToString() },
-				{ "u", options.UserId?.ToString() },
-				{ "type", options.Type },
-				{ "m", ((int?)options.Mode)?.ToString() },
-				{ "a", options.ConvertedBeatmaps == true ? "1" : "0" },
-				{ "h", options.Hash },
-				{ "limit", options.Limit?.ToString() },
-				{ "mods", options.Mods?.ToString() }
-			};
-			return get<Beatmap[]>(url, query.Where(kv => kv.Value != null));
+			return get<Beatmap[]>(url, BeatmapQuery(options));
 		}
 
         /// <summary>
@@ -86,34 +150,7 @@ namespace OsuNet {
         [Obsolete("Use GetUserAsync instead")]
         public User[] GetUser(GetUserOptions options) {
             string url = "https://osu.ppy.sh/api/get_user";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "u", options.User.ToString() },
-                { "m", ((int)options.Mode).ToString() },
-                { "type", options.Type },
-                { "event_days", options.EventDays?.ToString() }
-            };
-            return get<User[]>(url, query.Where(kv => kv.Value != null));
-        }
-
-        /// <summary>
-        /// Retrieve information about the top 100 scores of a specified beatmap.
-        /// </summary>
-        /// <param name="options"></param>
-        /// <returns>Array of scores.</returns>
-        [Obsolete("Use GetScoreAsync instead")]
-        public Scores[] GetScore(GetScoresOptions options) {
-            string url = "https://osu.ppy.sh/api/get_scores";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "b", options.BeatmapId?.ToString() },
-                { "u", options.User },
-                { "m", ((int)options.Mode).ToString() },
-                { "mods", options.Mods?.ToString() },
-                { "type", options.Type},
-                { "limit", options.Limit?.ToString() }
-            };
-            return get<Scores[]>(url, query.Where(kv => kv.Value != null));
+            return get<User[]>(url, UserQuery(options));
         }
 
         /// <summary>
@@ -124,14 +161,7 @@ namespace OsuNet {
         [Obsolete("Use GetUserBestAsync instead")]
         public UserBest[] GetUserBest(GetUserBestOptions options) {
             string url = "https://osu.ppy.sh/api/get_user_best";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "u", options.User },
-                { "m", ((int?)options.Mode)?.ToString() },
-                { "limit", options.Limit?.ToString() },
-                { "type", options.Type }
-            };
-            return get<UserBest[]>(url, query.Where(kv => kv.Value != null));
+            return get<UserBest[]>(url, UserBestQuery(options));
         }
 
         /// <summary>
@@ -142,14 +172,18 @@ namespace OsuNet {
         [Obsolete("Use GetUserRecentAsync instead")]
         public UserRecent[] GetUserRecent(GetUserRecentOptions options) {
             string url = "https://osu.ppy.sh/api/get_user_recent";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "u", options.User },
-                { "m", ((int?)options.Mode)?.ToString() },
-                { "limit", options.Limit?.ToString() },
-                { "type", options.Type}
-            };
-            return get<UserRecent[]>(url, query.Where(kv => kv.Value != null));
+            return get<UserRecent[]>(url, UserRecentQuery(options));
+        }
+
+        /// <summary>
+        /// Retrieve information about the top 100 scores of a specified beatmap.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns>Array of scores.</returns>
+        [Obsolete("Use GetScoreAsync instead")]
+        public Scores[] GetScores(GetScoresOptions options) {
+            string url = "https://osu.ppy.sh/api/get_scores";
+            return get<Scores[]>(url, ScoresQuery(options));
         }
 
         /// <summary>
@@ -160,11 +194,7 @@ namespace OsuNet {
         [Obsolete("Use GetMultiplayerAsync instead")]
         public Multiplayer GetMultiplayer(GetMultiplayerOptions options) {
             string url = "https://osu.ppy.sh/api/get_match";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "mp", options.MatchId.ToString() }
-            };
-            return get<Multiplayer>(url, query.Where(kv => kv.Value != null));
+            return get<Multiplayer>(url, MultiplayerQuery(options));
         }
 
         /// <summary>
@@ -175,16 +205,7 @@ namespace OsuNet {
         [Obsolete("Use GetReplayAsync instead")]
         public Replay GetReplay(GetReplayOptions options) {
             string url = "https://osu.ppy.sh/api/get_replay";
-            Dictionary<string, string> query = new Dictionary<string, string>() {
-                { "k", AccessToken },
-                { "b", options.BeatmapId.ToString() },
-                { "u", options.User },
-                { "m", ((int?)options.Mode)?.ToString() },
-                { "s", options.ScoreId },
-                { "type", options.Type },
-                { "mods", ((int?)options.Mods)?.ToString() }
-            };
-            return get<Replay>(url, query.Where(kv => kv.Value != null));
+            return get<Replay>(url, ReplayQuery(options));
         }
 
         /// <summary>
@@ -194,20 +215,7 @@ namespace OsuNet {
         /// <returns>Array of beatmap.</returns>
         public async Task<Beatmap[]> GetBeatmapAsync(GetBeatmapOptions options) {
 			string url = "https://osu.ppy.sh/api/get_beatmaps";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "since", options.Since?.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss") },
-				{ "s", options.BeatmapSetId?.ToString() },
-				{ "b", options.BeatmapId?.ToString() },
-				{ "u", options.UserId?.ToString() },
-				{ "type", options.Type },
-				{ "m", ((int?)options.Mode)?.ToString() },
-				{ "a", options.ConvertedBeatmaps == true ? "1" : "0" },
-				{ "h", options.Hash },
-				{ "limit", options.Limit?.ToString() },
-				{ "mods", options.Mods?.ToString() }
-			};
-			return await getAsync<Beatmap[]>(url, query.Where(kv => kv.Value != null));
+            return await getAsync<Beatmap[]>(url, BeatmapQuery(options));
 		}
 
 		/// <summary>
@@ -217,33 +225,7 @@ namespace OsuNet {
 		/// <returns>Array of user.</returns>
 		public async Task<User[]> GetUserAsync(GetUserOptions options) {
 			string url = "https://osu.ppy.sh/api/get_user";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "u", options.User.ToString() },
-				{ "m", ((int)options.Mode).ToString() },
-				{ "type", options.Type },
-				{ "event_days", options.EventDays?.ToString() }
-			};
-			return await getAsync<User[]>(url, query.Where(kv => kv.Value != null));
-		}
-
-		/// <summary>
-		/// Retrieve information about the top 100 scores of a specified beatmap.
-		/// </summary>
-		/// <param name="options"></param>
-		/// <returns>Array of scores.</returns>
-		public async Task<Scores[]> GetScoreAsync(GetScoresOptions options) {
-			string url = "https://osu.ppy.sh/api/get_scores";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "b", options.BeatmapId?.ToString() },
-				{ "u", options.User },
-				{ "m", ((int)options.Mode).ToString() },
-				{ "mods", options.Mods?.ToString() },
-				{ "type", options.Type},
-				{ "limit", options.Limit?.ToString() }
-			};
-			return await getAsync<Scores[]>(url, query.Where(kv => kv.Value != null));
+            return await getAsync<User[]>(url, UserQuery(options));
 		}
 
 		/// <summary>
@@ -253,14 +235,7 @@ namespace OsuNet {
 		/// <returns>Array of the user's best scores.</returns>
 		public async Task<UserBest[]> GetUserBestAsync(GetUserBestOptions options) {
 			string url = "https://osu.ppy.sh/api/get_user_best";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "u", options.User },
-				{ "m", ((int?)options.Mode)?.ToString() },
-				{ "limit", options.Limit?.ToString() },
-				{ "type", options.Type }
-			};
-			return await getAsync<UserBest[]>(url, query.Where(kv => kv.Value != null));
+			return await getAsync<UserBest[]>(url, UserBestQuery(options));
 		}
 
 		/// <summary>
@@ -270,28 +245,27 @@ namespace OsuNet {
 		/// <returns>Array of the user's recent results.</returns>
 		public async Task<UserRecent[]> GetUserRecentAsync(GetUserRecentOptions options) {
 			string url = "https://osu.ppy.sh/api/get_user_recent";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "u", options.User },
-				{ "m", ((int?)options.Mode)?.ToString() },
-				{ "limit", options.Limit?.ToString() },
-				{ "type", options.Type}
-			};
-			return await getAsync<UserRecent[]>(url, query.Where(kv => kv.Value != null));
+            return await getAsync<UserRecent[]>(url, UserRecentQuery(options));
 		}
 
-		/// <summary>
-		/// Retrieve information about a multiplayer match.
-		/// </summary>
-		/// <param name="options"></param>
-		/// <returns>Information about a multiplayer match.</returns>
-		public async Task<Multiplayer> GetMultiplayerAsync(GetMultiplayerOptions options) {
+        /// <summary>
+        /// Retrieve information about the top 100 scores of a specified beatmap.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns>Array of scores.</returns>
+        public async Task<Scores[]> GetScoresAsync(GetScoresOptions options) {
+            string url = "https://osu.ppy.sh/api/get_scores";
+            return await getAsync<Scores[]>(url, ScoresQuery(options));
+        }
+
+        /// <summary>
+        /// Retrieve information about a multiplayer match.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns>Information about a multiplayer match.</returns>
+        public async Task<Multiplayer> GetMultiplayerAsync(GetMultiplayerOptions options) {
 			string url = "https://osu.ppy.sh/api/get_match";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "mp", options.MatchId.ToString() }
-			};
-			return await getAsync<Multiplayer>(url, query.Where(kv => kv.Value != null));
+			return await getAsync<Multiplayer>(url, MultiplayerQuery(options));
 		}
 
 		/// <summary>
@@ -301,16 +275,7 @@ namespace OsuNet {
 		/// <returns>Replay data of a user's score on a map.</returns>
 		public async Task<Replay> GetReplayAsync(GetReplayOptions options) {
 			string url = "https://osu.ppy.sh/api/get_replay";
-			Dictionary<string, string> query = new Dictionary<string, string>() {
-				{ "k", AccessToken },
-				{ "b", options.BeatmapId.ToString() },
-				{ "u", options.User },
-				{ "m", ((int?)options.Mode)?.ToString() },
-				{ "s", options.ScoreId },
-				{ "type", options.Type },
-				{ "mods", ((int?)options.Mods)?.ToString() }
-			};
-			return await getAsync<Replay>(url, query.Where(kv => kv.Value != null));
+			return await getAsync<Replay>(url, ReplayQuery(options));
 		}
 	}
 }
